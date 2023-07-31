41d77e31234319e006d4a2e8f366b097100781c1
neoncargo <koalaresearch@proton.me>
2023-07-31 23:11:42 +0700

    [frontend] shitty search

--- static/index.js
+++ static/index.js
@@ -27,37 +27,79 @@ logoutBtn.addEventListener("click", () => {
     window.location.href = "/";
 });
 
-// "favorite" feature
-let favorite_checkboxes = document.getElementsByClassName("favorite");
+function hook_favorites() {
+    // "favorite" feature
+    let favorite_checkboxes = document.getElementsByClassName("favorite");
 
-for (const element of favorite_checkboxes) {
-    element.addEventListener("change", () => {
-        const XHR = new XMLHttpRequest();
+    for (const element of favorite_checkboxes) {
+        element.addEventListener("change", () => {
+            const XHR = new XMLHttpRequest();
 
-        XHR.addEventListener("load", (event) => {
-            if (XHR.status === 401) {
-                localStorage.removeItem("access_token");
-                console.log("got 401. Delete jwt");
-                SetInitialAuthBoxDisplay();
+            XHR.addEventListener("load", (event) => {
+                if (XHR.status === 401) {
+                    localStorage.removeItem("access_token");
+                    console.log("got 401. Delete jwt");
+                    SetInitialAuthBoxDisplay();
+                }
+            });
+
+            const jwt = localStorage.getItem("access_token");
+            const userLogged = (jwt && (jwt !== "undefined"));
+
+            if (!userLogged) {
+                console.log("You need to be logged");
+                return;
             }
+
+            if (element.checked) {
+                XHR.open("PUT", "/api/v1/me/favorites/" + element.value);
+            } else {
+                XHR.open("DELETE", "/api/v1/me/favorites/" + element.value);
+            }
+
+            XHR.setRequestHeader('Authorization', 'Bearer ' + jwt);
+
+            XHR.send();
         });
-
-        const jwt = localStorage.getItem("access_token");
-        const userLogged = (jwt && (jwt !== "undefined"));
-
-        if (!userLogged) {
-            console.log("You need to be logged");
-            return;
-        }
-
-        if (element.checked) {
-            XHR.open("PUT", "/api/v1/me/favorites/" + element.value);
-        } else {
-            XHR.open("DELETE", "/api/v1/me/favorites/" + element.value);
-        }
-
-        XHR.setRequestHeader('Authorization', 'Bearer ' + jwt);
-
-        XHR.send();
-    });
+    }
 }
+
+hook_favorites();
+
+const form = document.getElementById("form");
+const searchElem = document.querySelector("#search");
+const container = document.querySelector(".index-content");
+let oldContent = container.innerHTML;
+window.addEventListener("load", () => {
+    form.addEventListener("submit", async (event) => {
+        event.preventDefault();
+
+        let response = await fetch(
+            "/api/v1/search/movies?query=" + searchElem.value,
+        );
+        response = await response.json();
+
+        container.innerHTML = "";
+        for (const movie of response) {
+            container.innerHTML += `
+                <div class="movie-card">
+                    <a href="/movies/${movie.id}"><span></span></a>
+                    <div class="inner-movie">
+                    <input class="favorite" type="checkbox" name="favorite" value="${movie.id}">
+                    <div class="img-section"><img src="${movie.image_url}" alt=""></div>
+                    <p>${movie.title}</p>
+                    </div>
+                </div>
+            `;
+        }
+        hook_favorites();
+  });
+});
+
+// // on empty search
+searchElem.addEventListener("input", () => {
+    if (searchElem.value == "") {
+        container.innerHTML = oldContent;
+        hook_favorites();
+    }
+});
