750c62c38cef034b01a96373763347e3a69b0655
neoncargo <koalaresearch@proton.me>
2023-08-03 22:39:28 +0700

    [db/alembic] take DB URL from env.py which take it from db.database

--- alembic/env.py
+++ alembic/env.py
@@ -5,6 +5,8 @@ from sqlalchemy import pool
 
 from alembic import context
 
+from app.db.database import SQLALCHEMY_DATABASE_URL
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -19,6 +21,11 @@ if config.config_file_name is not None:
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
 target_metadata = None
+
+config.set_main_option(
+    "sqlalchemy.url",
+    SQLALCHEMY_DATABASE_URL
+)
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
