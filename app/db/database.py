b2a2931dc9f05347732bb9249ccc7637e80cfce6
neoncargo <koalaresearch@proton.me>
2023-08-03 22:34:09 +0700

    [app/db] take DB URL from env

--- app/db/database.py
+++ app/db/database.py
@@ -1,8 +1,9 @@
+import os
+
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = \
-    "postgresql://myuser:sd$2j6Asd34@localhost/top_movies"
+SQLALCHEMY_DATABASE_URL = os.environ["DB_URL"]
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
